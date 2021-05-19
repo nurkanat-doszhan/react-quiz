eff91fe23b0929a5e80ef47b560a7bf50814655c
nurkanat-doszhan <nurkanat.doszhan@gmail.com>
2021-05-19 19:55:50 +0600

    Список вопросов

---
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './AnswersList.css';
+import AnswerItem from './AnswerItem/AnswerItem'
+
+const AnswersList = props => (
+    <ul className={classes.AnswersList}>
+        <li>asd</li>
+    </ul>
+)
+
+export default AnswersList;
