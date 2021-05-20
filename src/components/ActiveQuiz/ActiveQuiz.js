c4eebab9b7f307574e6a35f3b4fdf0040a689b37
nurkanat-doszhan <nurkanat.doszhan@gmail.com>
2021-05-20 19:52:25 +0600

    Компонент кнопки

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -2,20 +2,24 @@ import React from 'react';
 import classes from './ActiveQuiz.css';
 import AnswersList from './AnswersList/AnswersList'
 
-const ActiveQuiz = props => (
+const ActiveQuiz = props => {
+    return (
     <div className={classes.ActiveQuiz}>
         <p className={classes.Question}>
             <span>
-                <strong>2.</strong>&nbsp;
-                Как дела?
+                <strong>{ props.answerNumber }.</strong>&nbsp;
+                {props.question}
             </span>
-            <small>2 из 12</small>
+            <small>{ props.answerNumber } из { props.quizLength }</small>
         </p>
 
-        <AnswersList 
+        <AnswersList
+            state={props.state}
             answers={props.answers}
+            onAnswerClick={props.onAnswerClick}
         />
     </div>
 )
+}
 
 export default ActiveQuiz;
